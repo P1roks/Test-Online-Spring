8b51af294de35f952c381617db7762e5092f43f7
P1roks <piotrekjakobczyk1@gmail.com>
2022-12-04 13:23:06 +0100

    TODO routes

--- src/main/java/com/Piroks/TestOnline/Controller/Controller.java
+++ src/main/java/com/Piroks/TestOnline/Controller/Controller.java
@@ -1,6 +1,10 @@
 package com.Piroks.TestOnline.Controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -10,4 +14,15 @@ public class Controller {
         return "index.html";
     }
 
+    @RequestMapping("/{questNo}")
+    public String test(@PathVariable(value="questNo") int number, Model model){
+       //TODO: Get variable number of questions and add them to the response before redirecting user to the website
+        return "TODO.html";
+    }
+
+    @PostMapping("/check")
+    public String check(Model model){
+        //TODO: Get all the answers to questions previously submitted by the user and then calculate the number of the correct answers
+        return "TODO.html";
+    }
 }
